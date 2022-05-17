baff64022be91396b348a0a8a7926a79ff84c23a
aquariusStudio <aquarius.github@gmail.com>
2022-05-17 19:27:53 +0900

    Update: Add comment.

--- source/org.aquarius.util/src/org/aquarius/util/mark/IUrlAcceptable.java
+++ source/org.aquarius.util/src/org/aquarius/util/mark/IUrlAcceptable.java
@@ -9,6 +9,12 @@ package org.aquarius.util.mark;
  */
 public interface IUrlAcceptable {
 
+	/**
+	 * Return whether the renamer can support the specified site.<BR>
+	 * 
+	 * @param urlString
+	 * @return
+	 */
 	public boolean isAcceptable(String urlString);
 
 }
