5e37465d6756f1b2512070a0e10723ad560feb49
aquariusStudio <aquarius.github@gmail.com>
2022-05-21 18:47:35 +0900

    Update: The site of "avhbo.com" changed.

--- source/org.aquarius.cicada.core/resources/config/parser/avhbo.com/list.js
+++ source/org.aquarius.cicada.core/resources/config/parser/avhbo.com/list.js
@@ -3,45 +3,59 @@ function internalParseMovieList() {
 	var movies = [];
 	result.movieList = movies;
 
-	var elementList = document.querySelectorAll("div.col-inner");
+	var elementList = document.querySelectorAll("div.post-item");
 
 	if (null != elementList) {
 		for (var i = 0; i < elementList.length; i++) {
 			var element = elementList[i];
 			var movie = new Object();
 
-			var titleElement = element.querySelector("h5.post-title");
+			var titleElement = element.querySelector(".post-title");
 
-			movie.title = titleElement.textContent;
+			movie.title = titleElement.innerText;
 
-			if (movie.title.indexOf("test") < 0) {
+			movie.title = movie.title.replace("\n\n", "");
+			movie.title = movie.title.replace("\n\n", "");
+			movie.title = movie.title.replace("\n\n", "");
+			movie.title = movie.title.replace("\n", "");
+			movie.title = movie.title.replace("\n", "");
+			movie.title = movie.title.replace("\n", "");
 
-				var linkElement = element.querySelector("a");
-				movie.pageUrl = linkElement.href;
+			var linkElement = element.querySelector("a");
+			movie.pageUrl = linkElement.href;
 
-				var imageElement = element.querySelector("img");
-				movie.imageUrl = imageElement.src;
+			var imageElement = element.querySelector("img");
+			movie.imageUrl = imageElement.src;
 
-				if (movie.imageUrl.indexOf("data") == 0) {
-					movie.imageUrl = imageElement.getAttribute("data-src")
-				}
-
-				movies.push(movie);
+			if (movie.imageUrl.indexOf("data") == 0) {
+				movie.imageUrl = imageElement.getAttribute("data-src");
 			}
+			
+			var durationElement=element.querySelector("span.text-nord5");
+			if(null!=durationElement)
+			{
+				movie.duration=durationElement.textContent;
+				movie.duration = movie.duration.replace("\n", "");
+				movie.duration = movie.duration.replace("\n", "");
+			}
+
+			movies.push(movie);
 		}
 	}
 
-	var pageElements = document.querySelectorAll("ul.nav-pagination > li");
-	var firstElement = pageElements[0];
-	var lastElement = pageElements[pageElements.length - 1];
-
-	if (lastElement.textContent.indexOf("»") < 0) {
-		result.lastPage = true;
-	} else {
-		lastElement = lastElement.previousElementSibling;
+	var pageFormElement = document.querySelector("span#price-currency");
+	if (null != pageFormElement) {
+		var formString = pageFormElement.textContent;
+		var stringIndex = formString.indexOf(" ");
+		formString = formString.substr(stringIndex);
+		result.totalPageCount = parseInt(formString);
 	}
 
-	result.totalPageCount = parseInt(lastElement.textContent);
+	var lastElement = document.querySelector("a[aria-label='pagination.next']");
+
+	if (lastElement == null) {
+		result.lastPage = true;
+	}
 
 	return result;
 }
